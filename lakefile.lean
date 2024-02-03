f6942f4a5942ff5430d68e454429a7c529c00368
JOSHCLUNE <josh.seth.clune@gmail.com>
2024-02-03 00:41:33 -0500

    Update to leanv4.6.0-rc1

--- lakefile.lean
+++ lakefile.lean
@@ -5,9 +5,9 @@ package «duperDemo» {
   -- add any package configuration options here
 }
 
-require «mathlib» from git "https://github.com/leanprover-community/mathlib4" @ "v4.4.0-rc1"
+require «mathlib» from git "https://github.com/leanprover-community/mathlib4" @ "v4.6.0-rc1"
 
-require «Duper» from git "https://github.com/leanprover-community/duper.git" @ "v0.0.5"
+require «Duper» from git "https://github.com/leanprover-community/duper.git" @ "v0.0.8"
 
 @[default_target]
 lean_lib «DuperDemo» {
